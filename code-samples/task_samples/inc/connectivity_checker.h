39c75a8b0f81665c5909626a4ef5bd4f444c3313
zetrus <morder_fun@mail.ru>
2026-02-02 19:38:54 +0300

    fix:сохранения

--- code-samples/task_samples/inc/connectivity_checker.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "generator_consts.h"
-#include "generator_objs.h"
-#include <stdio.h>
-
-int check_connectivity(dungeon_t *rooms);
-int depth_first_search(room_t *cur, int *visited);
--- code-samples/task_samples/inc/movement.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "walker_consts.h"
-#include "walker_objs.h"
-#include "generator_objs.h"
-
-void init_player(player_t *player, entity_t* spawn_point);
-void player_movement(map_t *map, player_t* player, int key);
